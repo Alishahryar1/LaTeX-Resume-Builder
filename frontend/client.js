8df160cfbeab981581feb47593c772c0b37cc931
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2024-03-06 14:04:08 -0800

    Final

--- frontend/client.js
+++ frontend/client.js
@@ -457,7 +457,7 @@ generateButton.addEventListener("click", async function (e) {
 	}
 
 	try {
-		const response = await fetch("http://localhost:3000/", {
+		const response = await fetch("https://latex-builder.onrender.com/", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
